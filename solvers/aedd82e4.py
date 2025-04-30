b5dd46384e9c3395645211b930bf95c7526395de
francescobraicovich <francesco.braicovich@gmail.com>
2025-04-30 18:11:20 +0200

    Add solve function to return encoded actions for challenge 1ef4f85a in aedd82e4.py

---
+++ solvers/aedd82e4.py
@@ -0,0 +1,22 @@
+from typing import List
+from action_space import ARCActionSpace
+EXCLUDE = {
+}
+
+def solve(space: ARCActionSpace) -> List[int]:
+    """
+    Return the exact sequence of encoded actions (ints) that solve challenge 1ef4f85a.
+    Use `space.encode(colour, select, transform)` (or whatever encode API you have)
+    to keep it human–readable.
+    """
+    file_name = __file__.split("/")[-1]
+    key = file_name.split(".")[0]
+    return [
+        # example:
+        space.encode(colour='most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+        space.encode(colour='most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+        space.encode(colour='second_most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+        space.encode(colour='second_most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+        space.encode(colour='second_most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+        space.encode(colour='second_most_independent_cells', selection="independent_cells_4", transform="new_colour_1", key=key),
+    ]
