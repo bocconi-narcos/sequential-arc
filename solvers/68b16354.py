a7f254759f0a698df01e9ff7fad1adf2694d7bcb
francescobraicovich <francesco.braicovich@gmail.com>
2025-04-30 18:10:54 +0200

    Add solve function to return encoded actions for challenge 1ef4f85a in 68b16354.py

---
+++ solvers/68b16354.py
@@ -0,0 +1,17 @@
+from typing import List
+from action_space import ARCActionSpace
+EXCLUDE = {
+}
+
+def solve(space: ARCActionSpace) -> List[int]:
+    """
+    Return the exact sequence of encoded actions (ints) that solve challenge 1ef4f85a.
+    Use `space.encode(colour, select, transform)` (or whatever encode API you have)
+    to keep it human–readable.
+    """
+    file_name = __file__.split("/")[-1]
+    key = file_name.split(".")[0]
+    return [
+        # example:
+        space.encode(colour='most_common', selection="all_cells", transform="flip_vertical", key=key),
+    ]
