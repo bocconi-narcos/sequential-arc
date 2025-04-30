4013ef5dda98e5e7e69ba5367f6aee771e7b9035
francescobraicovich <francesco.braicovich@gmail.com>
2025-04-30 18:11:00 +0200

    Add solve function to return encoded actions for challenge 1ef4f85a in 85c4e7cd.py

---
+++ solvers/85c4e7cd.py
@@ -0,0 +1,21 @@
+from typing import List
+from action_space import ARCActionSpace
+EXCLUDE = {
+    7, # Ambiguous change of dimension 
+    14, # Ambiguous change of dimension
+    22, # Ambiguous change of dimension
+    29 # Ambiguous change of dimension
+}
+
+def solve(space: ARCActionSpace) -> List[int]:
+    """
+    Return the exact sequence of encoded actions (ints) that solve challenge 1ef4f85a.
+    Use `space.encode(colour, select, transform)` (or whatever encode API you have)
+    to keep it human–readable.
+    """
+    file_name = __file__.split("/")[-1]
+    key = file_name.split(".")[0]
+    return [
+        # example:
+        space.encode(colour='most_common', selection="all_cells", transform="invert_colors", key=key),
+    ]
